cbe64c4d1b25f7f282a22e011fe72162d5d28640
80220 <43602145+80220@users.noreply.github.com>
2021-09-30 13:29:07 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,10 @@ let v;
 if (v === 3) {
   let a = 1;
   console.log(a());
+  const message = "Hello World!";
+  message.toLowerCase();
+  // Calling 'message'
+  message();
 }
 
 const rootElement = document.getElementById("root");
